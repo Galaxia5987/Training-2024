979b46a4541bc33aa2543ef4a3de6eed32add8be
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-11 13:57:02 +0300

    Create IntakeInputs.java

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeInputs.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class IntakeInputs {
+    public double angle = 0;
+    public double spinMotorPower = 0;
+    public double spinMotorCurrent = 0;
+
+    public double angleMotorSetpoint = 0;
+
+    public double spinMotorDesiredPower = 0;
+}
