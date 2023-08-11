6a8aa38f6fd4a3be5dd2985324da82ccc07f5b5e
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-11 13:56:19 +0300

    Create ShooterInputs.java

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterInputs.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class ShooterInputs {
+    public double motorVelocity = 0;
+    public double motor1Current = 0;
+    public double motor2Current = 0;
+    public double motor1BusVoltage = 0;
+    public double motor2BusVoltage = 0;
+    public double desiredMotorVelocity = 0;
+}
