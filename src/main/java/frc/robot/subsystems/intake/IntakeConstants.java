c4087945fc4849cb4ec5c2e0177569f8d4741a93
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-11 13:57:06 +0300

    Create IntakeConstants.java

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.intake;
+
+public class IntakeConstants {
+    public double stopCurrent = 12;
+    public static double kP = 1.3;
+    public static double kI = 0;
+    public static double kD = 0.1;
+    public static double kF = 0.1;
+
+}
