e7a5654750d2326d3b1e0a34a3e1d94c7771285e
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-11 13:56:45 +0300

    Create ShooterShoot.java

---
+++ src/main/java/frc/robot/commands/singlesystem/ShooterShoot.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.singlesystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.DoubleSupplier;
+
+public class ShooterShoot extends CommandBase {
+
+    private final DoubleSupplier supplier;
+    private final Shooter shooter = Shooter.getInstance();
+
+    public ShooterShoot(DoubleSupplier supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    public void execute() {
+        shooter.setShooterMotorVelocity(supplier.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.setShooterMotorVelocity(0);
+    }
+}
