c6f71ba36613fe4a837d058b62f97a1548d23530
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 18:56:19 +0300

    Create LiftUpCubeAndRelease.java

---
+++ src/main/java/frc/robot/commands/groups/LiftUpCubeAndRelease.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.groups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.singlesystem.ArmByPosition;
+import frc.robot.commands.singlesystem.ChangeGripperState;
+import frc.robot.subsystems.arm.ArmConstants;
+
+public class LiftUpCubeAndRelease extends SequentialCommandGroup {
+    public LiftUpCubeAndRelease(){
+        addCommands(
+                new ArmByPosition(ArmConstants.ELBOW_TOP_POSITION, ArmConstants.SHOULDER_TOP_POSITION),
+                new ChangeGripperState(false)
+        );
+    }
+}
