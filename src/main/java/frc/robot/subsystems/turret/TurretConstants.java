bdc177d6eb3dc398e998917c474f1854a51c8b80
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 17:25:25 +0300

    Create TurretConstants.java

---
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.turret;
+
+public class TurretConstants {
+    public static double kP = 0.7;
+    public static double kI = 0;
+    public static double kD = 0.1;
+    public static double kF = 0;
+}
