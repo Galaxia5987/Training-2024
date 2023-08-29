7a97f73fd322d9ac1c427a41b0b8f9e34411cf73
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 17:25:32 +0300

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Training-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "fcc2cb92d48146fa60bb4adf604a533a689daad1";
-  public static final String GIT_DATE = "2023-08-18 05:50:29 EDT";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "c97dd84396b70c831a9aa13f4d7eaf900106398e";
+  public static final String GIT_DATE = "2023-08-29 09:27:10 EDT";
   public static final String GIT_BRANCH = "first-subsystem";
-  public static final String BUILD_DATE = "2023-08-22 10:07:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1692713249242L;
+  public static final String BUILD_DATE = "2023-08-29 09:57:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1693317460250L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
