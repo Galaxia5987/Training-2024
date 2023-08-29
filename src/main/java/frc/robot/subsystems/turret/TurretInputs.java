40333135b3453605972a0afe1dcd9e493ce19cd2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 17:25:07 +0300

    Create TurretInputs.java

---
+++ src/main/java/frc/robot/subsystems/turret/TurretInputs.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.turret;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class TurretInputs {
+    public double turretAngle = 0;
+    public double turretPower = 0;
+    public double turretAngleSetPoint = 0;
+    public double turretPowerSetPoint = 0;
+
+    public double turretCurrent = 0;
+}
