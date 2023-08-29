78b38ae1a3075e25b08c79a1501f78a638f445fe
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 18:55:56 +0300

    Create GripperInputs.java

---
+++ src/main/java/frc/robot/subsystems/gripper/GripperInputs.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.gripper;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class GripperInputs {
+    public boolean isOpen;
+    public boolean wantedState;
+}
