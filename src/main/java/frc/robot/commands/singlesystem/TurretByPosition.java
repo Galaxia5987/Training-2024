25e7dd01abad0ecf135468e924531ed13b76d5fc
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-08-29 17:25:36 +0300

    Create TurretByPosition.java

---
+++ src/main/java/frc/robot/commands/singlesystem/TurretByPosition.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.singlesystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.turret.Turret;
+
+import java.util.Collections;
+import java.util.function.DoubleSupplier;
+
+public class TurretByPosition extends CommandBase {
+    private final Turret turret = Turret.getInstance();
+    private final double position;
+    public TurretByPosition(double position) {
+        this.position = position;
+    }
+
+    @Override
+    public void execute() {
+        turret.setTurretAngle(position);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        turret.setTurretPower(0);
+    }
+}
