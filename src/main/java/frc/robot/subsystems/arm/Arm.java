85206b5a00d57ac55ee123de319b34255e89ac91
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-01 12:58:20 +0300

    Update Arm.java

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -4,35 +4,25 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.utils.units.UnitModel;
 
 public class Arm extends SubsystemBase {
     private static Arm INSTANCE;
-    private final TalonFX elbowMotor;
-    private final TalonFX shoulderMotor;
-    private final UnitModel shoulderTicksToRads;
-    private final UnitModel elbowTicksToRads;
     private final ArmInputsAutoLogged inputs = new ArmInputsAutoLogged();
-
     private final ArmKinematics armKinematics = new ArmKinematics(ArmConstants.SHOULDER_LENGTH, ArmConstants.ELBOW_LENGTH);
 
+    private final ArmIO io;
     private ControlMode shoulderMode;
     private ControlMode elbowMode;
 
     private Arm() {
-        elbowMotor = new TalonFX(6);
-        shoulderMotor = new TalonFX(5);
-
-        elbowTicksToRads = new UnitModel(2048 / Math.PI * 2 / 51.3);
-        shoulderTicksToRads = new UnitModel(2048 / Math.PI * 2 / 106.7);
-
-        elbowMotor.config_kP(0, ArmConstants.elbowP, 100);
-        elbowMotor.config_kI(0, ArmConstants.elbowI, 100);
-        elbowMotor.config_kD(0, ArmConstants.elbowD, 100);
-
-        shoulderMotor.config_kP(0, ArmConstants.shoulderP, 100);
-        shoulderMotor.config_kI(0, ArmConstants.shoulderI, 100);
-        shoulderMotor.config_kD(0, ArmConstants.shoulderD, 100);
+        if (Robot.isReal()){
+            io = new ArmIOReal(5, 6);
+        }
+        else{
+            io = new ArmIOSim();
+        }
     }
 
     public static Arm getInstance() {
@@ -86,12 +76,17 @@ public class Arm extends SubsystemBase {
 
     @Override
     public void periodic() {
-        inputs.elbowPower = elbowMotor.getMotorOutputPercent();
-        inputs.shoulderPower = elbowMotor.getMotorOutputPercent();
-        inputs.elbowAngle = elbowTicksToRads.toUnits(elbowMotor.getSelectedSensorPosition());
-        inputs.shoulderAngle = shoulderTicksToRads.toUnits(shoulderMotor.getSelectedSensorPosition());
-
-        elbowMotor.set(elbowMode, (elbowMode == ControlMode.Position) ? inputs.elbowAngleSetPoint : inputs.elbowPowerSetPoint);
-        shoulderMotor.set(shoulderMode, (shoulderMode == ControlMode.Position) ? inputs.shoulderAngleSetPoint : inputs.shoulderPowerSetPoint);
+        if (elbowMode == ControlMode.Position) {
+            io.setElbowAngle(inputs.elbowAngle);
+        }
+        else if (elbowMode == ControlMode.PercentOutput){
+            io.setElbowPower(inputs.elbowPowerSetPoint);
+        }
+        if (shoulderMode == ControlMode.Position) {
+            io.setShoulderAngle(inputs.shoulderAngle);
+        }
+        else if (shoulderMode == ControlMode.PercentOutput){
+            io.setShoulderPower(inputs.shoulderPowerSetPoint);
+        }
     }
 }
