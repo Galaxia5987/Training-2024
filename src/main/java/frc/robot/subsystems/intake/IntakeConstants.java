95c046f495b08d1c19038f7a39852349899fa5bd
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-01 12:58:04 +0300

    Update IntakeConstants.java

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeConstants {
-    public double stopCurrent = 12;
-    public static double kP = 1.3;
+    public static final double SPIN_GEAR_RATIO = 35.26;
+    public static final double ANGLE_GEAR_RATIO = 10.0;
+    public static final double stopCurrent = 12;
+    public static final double kP = 1.3;
     public static double kI = 0;
     public static double kD = 0.1;
     public static double kF = 0.1;
