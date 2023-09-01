5e9d84ae91afde57b6a680e0bc397ab459e28582
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-01 12:57:58 +0300

    Update Intake.java

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -3,29 +3,22 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.utils.units.UnitModel;
 
 public class Intake extends SubsystemBase {
-    private final TalonFX angleMotor;
-    private final TalonFX spinMotor;
+    private final IntakeIO io;
     private static Intake INSTANCE;
-    private final UnitModel ticksToRads;
 
     private final IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
 
     private Intake(){
-        angleMotor = new TalonFX(1);
-        spinMotor = new TalonFX(2);
-        angleMotor.configFactoryDefault(10);
-        spinMotor.configFactoryDefault(10);
-
-        ticksToRads = new UnitModel(2048 / Math.PI * 2 / 0.2);
-
-        angleMotor.config_kP(0, IntakeConstants.kP, 10);
-        angleMotor.config_kI(0, IntakeConstants.kI, 10);
-        angleMotor.config_kD(0, IntakeConstants.kD, 10);
-        angleMotor.config_kF(0, IntakeConstants.kF, 10);
-
+        if (Robot.isReal()){
+            io = new IntakeIOReal(1, 2);
+        }
+        else{
+            io = new IntakeIOSim();
+        }
     }
 
     public static Intake getInstance(){
@@ -58,11 +51,9 @@ public class Intake extends SubsystemBase {
 
     @Override
     public void periodic() {
-        inputs.angle = ticksToRads.toUnits(angleMotor.getSelectedSensorPosition());
-        inputs.spinMotorPower = spinMotor.getMotorOutputPercent();
-        inputs.spinMotorCurrent = spinMotor.getStatorCurrent();
+        io.updateInputs(inputs);
 
-        spinMotor.set(ControlMode.PercentOutput, inputs.spinMotorDesiredPower);
-        angleMotor.set(ControlMode.Position, inputs.angleMotorSetpoint);
+        io.setAngleMotorAngle(inputs.angleMotorSetpoint);
+        io.setSpinMotorPower(inputs.spinMotorDesiredPower);
     }
 }
