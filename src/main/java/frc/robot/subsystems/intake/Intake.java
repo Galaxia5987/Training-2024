91a4a6a68ff44d3c39c45a797d001b848f236920
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-05 18:02:25 +0300

    Update Intake.java

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.utils.units.UnitModel;
 import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
     private final IntakeIO io;
     private static Intake INSTANCE;
+    private ControlMode angleMode;
 
     private final IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
 
@@ -41,12 +40,24 @@ public class Intake extends SubsystemBase {
         return inputs.spinMotorCurrent;
     }
 
+    public double getAngleMotorCurrent(){
+        return inputs.angleMotorCurrent;
+    }
+
     public void setSpinMotorPower(double power){
         inputs.spinMotorDesiredPower = power;
+    }
+    public void setAngleMotorPower(double power){
+        inputs.angleMotorWantedPower = power;
+        angleMode = ControlMode.PercentOutput;
     }
 
     public void setAngleMotorAngle(double angle){
         inputs.angleMotorSetpoint = angle;
+        angleMode = ControlMode.Position;
+    }
+    public void resetAngleMotor(){
+        io.resetIntake();
     }
 
 
@@ -55,8 +66,13 @@ public class Intake extends SubsystemBase {
         io.updateInputs(inputs);
         Logger.getInstance().processInputs("Intake", inputs);
 
+        if (angleMode == ControlMode.Position) {
+            io.setAngleMotorAngle(inputs.angleMotorSetpoint);
+        }
+        else if (angleMode == ControlMode.PercentOutput){
+            io.setAngleMotorPower(inputs.angleMotorWantedPower);
+        }
 
-        io.setAngleMotorAngle(inputs.angleMotorSetpoint);
         io.setSpinMotorPower(inputs.spinMotorDesiredPower);
     }
 }
