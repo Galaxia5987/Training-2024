f07acea8223c468bde566f7c4edd374d294416cb
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-05 18:02:43 +0300

    Update IntakePickupCube.java

--- src/main/java/frc/robot/commands/singlesystem/IntakePickupCube.java
+++ src/main/java/frc/robot/commands/singlesystem/IntakePickupCube.java
@@ -2,9 +2,14 @@ package frc.robot.commands.singlesystem;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.intake.Intake;
+import org.eclipse.jetty.server.PushBuilder;
 
 public class IntakePickupCube extends CommandBase {
     private final Intake intake = Intake.getInstance();
+
+    public IntakePickupCube(){
+        addRequirements(intake);
+    }
 
     @Override
     public void execute() {
