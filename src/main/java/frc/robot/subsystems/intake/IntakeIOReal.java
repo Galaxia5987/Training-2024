04730bcb32a68e83a13ef8c95d8b8228c8ccb198
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-09-05 18:02:31 +0300

    Update IntakeIOReal.java

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -2,21 +2,22 @@ package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import frc.robot.utils.units.UnitModel;
 
 public class IntakeIOReal implements IntakeIO{
     private final TalonFX angleMotor;
-    private final TalonFX spinMotor;
+    private final CANSparkMax spinMotor;
     private final UnitModel ticksToRads;
 
     public IntakeIOReal(int angleMotorID, int spinMotorID){
         angleMotor = new TalonFX(angleMotorID);
-        spinMotor = new TalonFX(spinMotorID);
+        spinMotor = new CANSparkMax(spinMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
         angleMotor.configFactoryDefault(10);
-        spinMotor.configFactoryDefault(10);
-
         ticksToRads = new UnitModel(2048 / Math.PI * 2 / 0.2);
 
+        angleMotor.setInverted(false);
         angleMotor.config_kP(0, IntakeConstants.kP, 10);
         angleMotor.config_kI(0, IntakeConstants.kI, 10);
         angleMotor.config_kD(0, IntakeConstants.kD, 10);
@@ -24,7 +25,7 @@ public class IntakeIOReal implements IntakeIO{
     }
     @Override
     public void setSpinMotorPower(double power) {
-        spinMotor.set(ControlMode.PercentOutput, power);
+        spinMotor.set(power);
     }
 
     @Override
@@ -32,9 +33,20 @@ public class IntakeIOReal implements IntakeIO{
         angleMotor.set(ControlMode.Position, angle);
     }
 
+    @Override
+    public void setAngleMotorPower(double power) {
+        angleMotor.set(ControlMode.PercentOutput, power);
+    }
+
+    @Override
+    public void resetIntake() {
+        angleMotor.setSelectedSensorPosition(0);
+    }
+
     public void updateInputs(IntakeInputs inputs){
+        inputs.angleMotorCurrent = angleMotor.getStatorCurrent();
         inputs.angle = ticksToRads.toUnits(angleMotor.getSelectedSensorPosition());
-        inputs.spinMotorPower = spinMotor.getMotorOutputPercent();
-        inputs.spinMotorCurrent = spinMotor.getStatorCurrent();
+        inputs.spinMotorPower = spinMotor.getAppliedOutput();
+        inputs.spinMotorCurrent = spinMotor.getOutputCurrent();
     }
 }
