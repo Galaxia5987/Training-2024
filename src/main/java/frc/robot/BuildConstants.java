616dac70257dd71a8e06314f09245f54eb0a76ff
Alon Schwierz <@1>
2023-09-12 16:53:14 +0300

    .

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Training-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 86;
-  public static final String GIT_SHA = "e45686dcb2902503a84c1f1aa2391f00ee39bd45";
-  public static final String GIT_DATE = "2023-09-05 11:02:52 EDT";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "72c96b0d836c688e7c7b77e166ef87f30657c9d3";
+  public static final String GIT_DATE = "2023-09-05 11:31:11 EDT";
   public static final String GIT_BRANCH = "first-subsystem";
-  public static final String BUILD_DATE = "2023-09-05 11:29:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1693927787952L;
+  public static final String BUILD_DATE = "2023-09-12 09:50:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1694526609614L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -1,22 +1,22 @@
-//package frc.robot.subsystems.arm;
-//
-//import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-//import edu.wpi.first.math.geometry.Translation2d;
-//
-//public class ArmConstants {
-//    public static final double SHOULDER_LENGTH = 0.75679; // [m]
-//    public static final double ELBOW_LENGTH = 0.75889; // [m]
-//
-//    public static final double ELBOW_ZERO_POSITION = 360 - 53.33; //[degrees]
-//    public static final double SHOULDER_ZERO_POSITION = 180 - 65.53; //[degrees]
-//    public static final double ELBOW_TOP_POSITION = 210; //[degrees]
-//    public static final double SHOULDER_TOP_POSITION = 120; //[degrees]
-//
-//    //PID
-//    public static final double shoulderP = 0.1;
-//    public static final double shoulderI = 0;
-//    public static final double shoulderD = 0;
-//    public static final double elbowP = 0;
-//    public static final double elbowI = 0.0;
-//    public static final double elbowD = 0.0;
-//}
+package frc.robot.subsystems.arm;
+
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class ArmConstants {
+    public static final double SHOULDER_LENGTH = 0.75679; // [m]
+    public static final double ELBOW_LENGTH = 0.75889; // [m]
+
+    public static final double ELBOW_ZERO_POSITION = 360 - 53.33; //[degrees]
+    public static final double SHOULDER_ZERO_POSITION = 180 - 65.53; //[degrees]
+    public static final double ELBOW_TOP_POSITION = 210; //[degrees]
+    public static final double SHOULDER_TOP_POSITION = 120; //[degrees]
+
+    //PID
+    public static final double shoulderP = 0.1;
+    public static final double shoulderI = 0;
+    public static final double shoulderD = 0;
+    public static final double elbowP = 0;
+    public static final double elbowI = 0.0;
+    public static final double elbowD = 0.0;
+}
